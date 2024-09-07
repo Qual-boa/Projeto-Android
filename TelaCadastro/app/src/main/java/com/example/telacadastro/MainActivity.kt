3754e8a77039505ff66592282147cafb0f907843
danielleromano <danielle.romano@sptech.school>
2024-09-07 01:40:56 -0300

    tela de cadastro

---
+++ TelaCadastro/app/src/main/java/com/example/telacadastro/MainActivity.kt
@@ -0,0 +1,199 @@
+package com.example.registration
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.telacadastro.R
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val montserrat = GoogleFont(name = "Montserrat")
+val montserratFamily = FontFamily(
+    Font(googleFont = montserrat, fontProvider = provider, weight = FontWeight.Normal),
+    Font(googleFont = montserrat, fontProvider = provider, weight = FontWeight.Bold)
+)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RegistrationScreen()
+        }
+    }
+}
+
+@Composable
+fun RegistrationScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFE5B4))
+    ) {
+        Image(
+            painter = painterResource(id = R.mipmap.background),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.mipmap.icon),
+                contentDescription = "User Icon",
+                modifier = Modifier
+                    .size(200.dp)
+                    .align(Alignment.CenterHorizontally) // Alinha a imagem no centro
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "CADASTRO",
+                color = Color.Black,
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = montserratFamily,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            InputFieldWithShadow(label = "Nome:", text = "")
+            Spacer(modifier = Modifier.height(16.dp))
+
+            InputFieldWithShadow(label = "E-mail:", text = "")
+            Spacer(modifier = Modifier.height(16.dp))
+
+            InputFieldWithShadow(label = "Senha:", text = "", isPassword = true)
+            Spacer(modifier = Modifier.height(16.dp))
+
+            InputFieldWithShadow(label = "Confirmar Senha:", text = "", isPassword = true)
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            // Botões
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(end = 8.dp)
+                        .clip(RoundedCornerShape(50.dp)),
+                    colors = androidx.compose.material3.ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFFFEBA4)
+                    )
+                ) {
+                    Text(text = "ENTRAR", color = Color.Black, fontFamily = montserratFamily,
+                        fontSize = 18.sp)
+                }
+
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 8.dp)
+                        .clip(RoundedCornerShape(50.dp)),
+                    colors = androidx.compose.material3.ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFBD5A0D)
+                    )
+                ) {
+                    Text(text = "CADASTRAR", color = Color.Black, fontFamily = montserratFamily,
+                        fontSize = 18.sp)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun InputFieldWithShadow(label: String, text: String, isPassword: Boolean = false) {
+    val inputValue = remember { mutableStateOf(text) }
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = label,
+            style = TextStyle(
+                fontSize = 16.sp,
+                color = Color.Black,
+                fontFamily = montserratFamily
+            ),
+            modifier = Modifier.padding(start = 8.dp, bottom = 4.dp)
+        )
+
+        Box(
+            modifier = Modifier
+                .height(46.dp)
+                .shadow(
+                    elevation = 8.dp,
+                    shape = RoundedCornerShape(20.dp),
+                    ambientColor = Color(0xFFFFA726),
+                    spotColor = Color(0xFFFFA726)
+                ) // Sombra inferior e à direita
+                .clip(RoundedCornerShape(24.dp))
+                .background(Color.White) // Fundo branco
+                .padding(4.dp)
+        ) {
+            BasicTextField(
+                value = inputValue.value,
+                onValueChange = { inputValue.value = it },
+                visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+                textStyle = TextStyle(color = Color.Black, fontSize = 16.sp, fontFamily = montserratFamily),
+                cursorBrush = SolidColor(Color.Black),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 12.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegistrationScreenPreview() {
+    RegistrationScreen()
+}
+
+//jshdh
